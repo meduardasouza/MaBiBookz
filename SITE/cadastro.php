6b55428b6d67da1de4500570ebf9cedb4cc53bd8
Maya <mayararegina555@gmail.com>
2018-06-21 09:23:45 -0300

    update new files 21/06/18

---
+++ SITE/cadastro.php
@@ -0,0 +1,38 @@
+<?php
+
+	$con = mysqli_connect('127.0.0.1','root', 'root');
+	//Coloca os coisas ali...No meio de '' coloquem a "root"...Isso se vocês estiverem no pc da escola
+	if(!$con)
+	{
+		echo 'deu ruim';
+	}
+	//Coloquem o nome do BD que esta entre "test";
+	if(!mysqli_select_db($con, 'MaBiBookz'))
+	{
+		echo 'BD não selecionado';
+	}
+	//Criem strings e coloquem o mesmo nome do BD
+	$nome = $_POST['nome_compl'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+	
+
+	
+	//coloquem o nome da tabela que quer cadastrar depois de usuario e depois os nomes das strings e depois de VALUES coloquem as strings criadas
+	$sql = "INSERT INTO usuario (nome_compl, email, senha ) VALUES ('$nome', '$email', '$senha')";
+	
+	if(!mysqli_query($con,$sql))
+	{
+		echo 'não cadastrado';
+	}
+	else 
+	{
+		echo 'Cadastrado com sucesso'; 
+		header("Location: login.php");// A pagina que vocês querem ir(recomendo que seja a de LOGIN)
+	}
+	
+	//header("refresh:;2 url=inicio.html");
+
+	//By volei y google
+	
+?>
