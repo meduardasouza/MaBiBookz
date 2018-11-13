f438ccf4f087d3b2c6d3bdc7d3831a8eadf30deb
meduardasouza <megs.dlt12@gmail.com>
2018-11-13 10:02:24 -0200

    Link no perfil consertado

---
+++ SITE/rastreio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<!--
+    Introspect by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+    <head>
+        <title> Erro </title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+    </head>
+    <body>
+
+        <!-- Header -->
+  <?php include ("cabeçalhosc.php") ?>
+                    <section id="main">
+                <div class="inner">
+                    <header class="major special">
+                        <!-- Form -->
+                        <section>
+                            <h3>Ops!¯\_(ツ)_/¯</h3>
+                            <b>Verifique se os dados foram preenchidos corretamente e tente de novo.</b>
+                                        </section>        
+<br><br><br><br><br>
+
+
+      <?php include ("rodape.php") ?>
+        <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/skel.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
+       </body>
+</html>
